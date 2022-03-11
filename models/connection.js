e8ddab5639ecb47371509cd19d17d188f0f456f2
JulietteCoemelck <juliette.coemelck@gmail.com>
2022-03-11 18:44:13 +0100

    first commit

--- models/connection.js
+++ models/connection.js
@@ -2,12 +2,14 @@ var mongoose = require('mongoose');
 /* const dotenv = require("dotenv");
 dotenv.config(); */
 
+const BDD_LINK = process.env.BDD_LINK;
+
 var options = {
  connectTimeoutMS: 5000,
  useNewUrlParser: true,
  useUnifiedTopology : true
 }
-mongoose.connect(`${process.env.BDD_LINK}`, options, function(err) { console.log(err);
+mongoose.connect(`${BDD_LINK}`, options, function(err) { console.log(err);
  }
 );
 
