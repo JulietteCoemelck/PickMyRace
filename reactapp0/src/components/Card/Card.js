06db851050640e3471ae9d337ceac565a72727e4
JulietteCoemelck <juliette.coemelck@gmail.com>
2022-03-15 12:36:43 +0100

    touch circuit done

--- reactapp0/src/components/Card/Card.js
+++ reactapp0/src/components/Card/Card.js
@@ -1,25 +1,32 @@
+import { useState } from 'react';
 import './Card.css';
 
 export default function Card(props){
-    // const imageCategorie = require(`../media/${props.img}`);
+    const [circuitStyle, setCircuitStyle] = useState(false);
     const circuitImg = require(`../media/circuitImg/${props.circuitImg}`);
     const circuitName = props.circuitName;
     const cupImg = require(`../media/cupImg/${props.cupImg}`);
     const cupName = props.cupName;
 
+
     return (
-        <div className='Card'>
+        <div className='Card' onClick={() => setCircuitStyle(!circuitStyle)}>
             <img src={circuitImg} alt='' className='circuitImg'/>
             <div className='circuitInfo'>
                 <div className='circuitCup'>
-                    <h4>Coupe {cupName}</h4>
                     <img src={cupImg} alt='' className='cupImg'/>
+                    <h4>Coupe {cupName}</h4>
+                    
                 </div>
                 <div className='circuitName'>
                     <h3>{circuitName}</h3>
                 </div>
             </div>
-
+            {circuitStyle ? (
+                <div className='circuitDone'>
+                    <h3>Terminé !</h3>
+                </div>
+            ) : (null)}
         </div>
     )
 }
